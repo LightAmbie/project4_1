1657acabaeedce24b67cff567b8f76351e778ccc
LightAmbie <lightambie@gmail.com>
2022-09-19 21:27:45 +0300

    Initial commit

---
+++ src/mirea/ru/lazarev_pavel/kvbo0121/Circle.java
@@ -0,0 +1,46 @@
+package mirea.ru.lazarev_pavel.kvbo0121;
+
+public class Circle extends Shape{
+    protected double radius;
+
+    Circle(){
+
+    }
+
+    public Circle(double radius){
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color, boolean filled){
+        this.radius = radius;
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    double getArea() {
+        return 3.14 * radius * radius;
+    }
+
+    @Override
+    double getPerimeter() {
+        return 2 * 3.14 * radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                ", color='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
+}
---
+++ src/mirea/ru/lazarev_pavel/kvbo0121/Square.java
@@ -0,0 +1,48 @@
+package mirea.ru.lazarev_pavel.kvbo0121;
+
+public class Square extends Rectangle{
+    protected double side;
+
+    Square(){
+
+    }
+
+    public Square(double side){
+        this.side = side;
+    }
+
+    public Square(double side, String color, boolean filled){
+        this.side = side;
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        this.side = side;
+    }
+
+    @Override
+    public double getLength() {
+        return super.getLength();
+    }
+
+    @Override
+    public void setWidth(double width) {
+        super.setWidth(width);
+    }
+
+    @Override
+    public String toString() {
+        return "Square{" +
+                "side=" + side +
+                ", width=" + width +
+                ", length=" + length +
+                ", color='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
+}
